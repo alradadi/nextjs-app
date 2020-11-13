7298d3d2edb7cf700daf8d20d04e382fa4af1e85
Abdul Alradadi <abdul@radadi.me>
2020-11-13 21:54:26 +0300

    added eslint/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'rn-ts',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-raw-text': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'off',
+  },
+};
